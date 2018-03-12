e89c42ce71ed36e86c0624d9643d8ba1dc97f134
8bithero <pkemanes@gmail.com>
2018-03-12 22:06:51 +0000

    Pushing to github

--- web/router.ex
+++ web/router.ex
@@ -23,7 +23,13 @@ defmodule Discuss.Router do
     # put "/topics/:id", TopicController, :update
     # delete "/topics/:id", TopicController, :delete
     resources "/", TopicController
+  end
 
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
   end
 
   # Other scopes may use custom stacks.
